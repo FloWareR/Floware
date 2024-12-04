0574ae9ffe39ce465b362b66e28db35000174353
Floware <rafaxtz3@gmail.com>
2024-12-03 18:22:55 -0600

    no message

--- app/Models/OrderItems.php
+++ app/Models/OrderItems.php
@@ -18,14 +18,15 @@ class OrderItems extends Model{
   public function readByOrderId($params){
     try {
       $query = "SELECT 
-                      order_items.product_id, 
-                      products.name, 
-                      order_items.quantity, 
-                      order_items.price, 
-                      order_items.total
+                      {$this->table}.product_id, 
+                      Products.name, 
+                      {$this->table}.quantity, 
+                      {$this->table}.price, 
+                      {$this->table}.total
                     FROM  {$this->table}
-                    JOIN Products ON products.id = order_items.product_id
-                    WHERE order_items.order_id = :order_id;";
+                    JOIN Products ON products.id = {$this->table}.product_id
+                    WHERE {$this->table}.order_id = :order_id;";
+
       $stmt = $this->db->prepare($query);
       $stmt->bindParam(':order_id', $params, Helper::getParamType($params));
       $stmt->execute();
